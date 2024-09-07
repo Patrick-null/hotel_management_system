3bb903ebd8809db110b68feaf997c4ffa26fbfb6
Patrick <18365917255@163.com>
2024-09-07 14:29:41 +0800

    创建好admin

---
+++ hotel_management_system/src/main/java/com/patrick/bean/Admin.java
@@ -0,0 +1,16 @@
+package com.patrick.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+    private Integer aid;
+    private String username;
+    private String password;
+    //角色 0:管理员 1:用户
+    private Integer role;
+}
